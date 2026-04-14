929121c955df64f36c442c4bbeb656830905efdd
oumnia <ahlem.elmokretar@ynov.com>
2026-04-14 22:37:36 +0200

    étudiant2: workflow, services métier, command et upload

---
+++ backend/src/Entity/Project.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[Vich\Uploadable]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private string $state = 'draft';
+
+    #[ORM\Column]
+    private float $goalAmount = 0.0;
+
+    #[ORM\Column]
+    private float $currentAmount = 0.0;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[Vich\UploadableField(mapping: 'project_images', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getGoalAmount(): float
+    {
+        return $this->goalAmount;
+    }
+
+    public function setGoalAmount(float $goalAmount): static
+    {
+        $this->goalAmount = $goalAmount;
+
+        return $this;
+    }
+
+    public function getCurrentAmount(): float
+    {
+        return $this->currentAmount;
+    }
+
+    public function setCurrentAmount(float $currentAmount): static
+    {
+        $this->currentAmount = $currentAmount;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): static
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+}
